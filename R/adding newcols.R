6e47788b05c2d808d60ce8d6b31b29cd8f463b00
Erendira <erendiraaceves@Erendiras-MacBook-Pro.local>
2017-11-13 16:10:46 -0800

    Code

--- R/adding newcols.R
+++ R/adding newcols.R
@@ -1,6 +1,7 @@
-newcol <- read.csv("newcols.csv")
+
 library(readr)
 cooperatives_clean <- read_csv("cooperatives_clean.csv")
+newcol <- read.csv("newcols.csv")
 cooperatives_newcols <- merge(cooperatives_clean,newcol)
 
 write.csv(cooperatives_newcols,file="cooperatives_newcols")
