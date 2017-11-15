ad04a42387ed167a22bc7170b1ae514ba06116d5
jcvdav <juancarlos.villader@gmail.com>
2017-11-14 18:55:45 -0800

    Added new variables to the database #6 but still need to define what to do with multiple values for a single fishery

--- R/adding newcols.R
+++
@@ -1,7 +0,0 @@
-
-library(readr)
-cooperatives_clean <- read_csv("cooperatives_clean.csv")
-newcol <- read.csv("newcols.csv")
-cooperatives_newcols <- merge(cooperatives_clean,newcol)
-
-write.csv(cooperatives_newcols,file="cooperatives_newcols")
